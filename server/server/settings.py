02fc2a475ca2d2aaa6e631844b328affe602296d
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-02-12 11:40:42 -0800

    made the models for array

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'insert2DB.apps.Insert2DbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
