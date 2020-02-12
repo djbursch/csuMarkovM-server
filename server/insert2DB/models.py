b86f0db71164edb0b067346e97bc93b0a48f3857
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-02-12 12:03:23 -0800

    added admin capabilities

--- server/insert2DB/models.py
+++ server/insert2DB/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Array(models.Model):
 	array_num = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.array_num
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
