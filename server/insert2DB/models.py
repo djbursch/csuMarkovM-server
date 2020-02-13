670c60c0d8eccb5b8d5f79a7c2565e2ec4cf0375
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-02-13 13:17:22 -0800

    just further designing the pipes

--- server/insert2DB/models.py
+++ server/insert2DB/models.py
@@ -2,9 +2,9 @@ from django.db import models
 from django.utils import timezone
 
 class Array(models.Model):
+	#GOING TO CHANGE THIS TO FileField when getting actual data
 	array_num = models.CharField(max_length = 200)
 	pub_date = models.DateTimeField('date published')
-	#std_dev = models.IntegerField(1)
 	def __str__(self):
 		return self.array_num
 	def was_published_recently(self):
--- server/insert2DB/oracle.py
+++ server/insert2DB/oracle.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from .models import Array
+
+#This is where the matlab code is going to be translated
+#Just created the pipeline so it's ready to receive data
 
 def Oracle(array_id):
 	array = Array.objects.get(id=array_id)
