4866ff38db444be71a3a78ed562cf65bbbcddee2
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-02-14 15:50:07 -0800

    cleaning up

--- server/insert2DB/oracle.py
+++ server/insert2DB/oracle.py
@@ -5,7 +5,12 @@ import random as rm
 #This is where the matlab code is going to be translated
 #Just created the pipeline so it's ready to receive data
 
-def Oracle(schoolName):
-	data = Data.objects.get(schoolName=schoolName)
-	print(data)
-	return data
+def Oracle(request):
+	#This is for when the model is already trained
+	markovModel = 1
+	return markovModel
+
+def oracleTrain(data):
+	#Do the training calculations in here
+	markovModel = 1
+	return markovModel
--- server/insert2DB/views.py
+++ server/insert2DB/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from .models import Data
 from django.shortcuts import render
 from django.utils import timezone
-from .oracle import Oracle
+from .oracle import Oracle, oracleTrain
 
 #For getting all items in data collection
 def index(request):
@@ -17,11 +17,13 @@ def singleData(request, schoolName):
 
 #Create new data for a school in collection
 def createData(request):
-	newData = Data(data=request.POST.get('data'), schoolName=request.POST.get('schoolName'), departmentName=request.POST.get('departmentName'), pubDate=timezone.now())
+	data = request.POST.get('data')
+	markovModel = oracleTrain(data)
+	newData = Data(data=request.POST.get('data'), schoolName=request.POST.get('schoolName'), departmentName=request.POST.get('departmentName'), markovModel=markovModel, pubDate=timezone.now())
 	newData.save()
 	return HttpResponse(newData)
 
 #Send a schools data to the oracle
 def oracle(request, schoolName):
-	output = Oracle(schoolName)
+	output = Oracle(request)
 	return HttpResponse(output)
