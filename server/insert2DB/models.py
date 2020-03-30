76a39d9960e692f5461a30192270c9b6cae046a0
Jackson Bursch <jacksonbursch@Jacksons-MacBook-Pro.local>
2020-03-29 18:31:16 -0700

    adding the requirements page

--- server/insert2DB/models.py
+++ server/insert2DB/models.py
@@ -25,38 +25,38 @@ class Invite(models.Model):
 #CONSUMER ROLES
 class DeptConsumer(User):
 	class Meta:
-		permissions = (("is_member", "can_view"),)
+		permissions = (("can_read_dpt", "Friendly message"),)
 
 class CollegeConsumer(DeptConsumer):
     class Meta:
-    	permissions = (("can_upload", "Friendly message"),)
+    	permissions = (("can_read_clg", "Friendly message"),)
 
 class UnivConsumer(CollegeConsumer):
     class Meta:
-    	permissions = (("can_view_school", "Friendly message"),)
+    	permissions = (("can_read_uni", "Friendly message"),)
 
 class SystemConsumer(UnivConsumer):
 	class Meta:
-		permissions = (("can_view_all", "Friendly message"),)
+		permissions = (("can_read_sys", "Friendly message"),)
 
 # PROVIDER ROLES
 class DeptProvider(DeptConsumer):
 	class Meta:
-		permissions = (("can_view_dpt", "Friendly message"),)
+		permissions = (("can_write_dpt", "Friendly message"),)
 
 class CollegeProvider(DeptProvider):
 	class Meta:
-		permissions = (("can_view_clg", "Friendly message"),)
+		permissions = (("can_write_clg", "Friendly message"),)
 
 class UnivProvider(CollegeProvider):
 	class Meta:
-		permissions = (("can_view_all", "Friendly message"),)
+		permissions = (("can_write_uni", "Friendly message"),)
 
 class SystemProvider(UnivProvider):
 	class Meta:
-		permissions = (("can_view_all", "Friendly message"),)
+		permissions = (("can_write_sys", "Friendly message"),)
 
 #PROJECT OWNERS
 class Developer(SystemProvider):
 	class Meta:
-		permissions = (("can_view_all", "Friendly message"),)
+		permissions = (("can_view_all", "can_write_all"),)
